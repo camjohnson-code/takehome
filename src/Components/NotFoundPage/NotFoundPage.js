e386fcec13dedab66a29c9a937044d95952438e6
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-03-14 15:23:20 -0600

    Add not found page

---
+++ src/Components/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,12 @@
+import './NotFoundPage.css';
+
+const NotFoundPage = () => {
+  return (
+    <section className='not-found'>
+      <h1 className='not-found-title'>404</h1>
+      <p className='not-found-text'>Page not found</p>
+    </section>
+  );
+};
+
+export default NotFoundPage;
